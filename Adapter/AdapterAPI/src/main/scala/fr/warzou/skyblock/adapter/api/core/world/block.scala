dce22deef33126cc8a7031037a5d637112f3adb9
WarzouMc <warzoumc@gmail.com>
2022-08-13 23:06:35 +0200

    Block scaladoc

--- Adapter/AdapterAPI/src/main/scala/fr/warzou/skyblock/adapter/api/core/world/block.scala
+++ Adapter/AdapterAPI/src/main/scala/fr/warzou/skyblock/adapter/api/core/world/block.scala
@@ -2,21 +2,40 @@ package fr.warzou.skyblock.adapter.api.core.world
 
 import fr.warzou.skyblock.adapter.api.common.wrap.{Unwrapper, Wrappable, Wrapper}
 
+/**
+ * A wrap of minecraft block
+ *
+ * @version 0.0.1
+ * @author Warzou
+ */
 trait Block extends Wrappable[Block] {
 
+  /**
+   * @return block type name
+   */
   def name: String
 
+  /**
+   * @return block data
+   */
   def data: Int
 
+  /**
+   * @return block is a tile entity
+   */
   def isBlockEntity: Boolean
 
+  /**
+   * @return a NBTTagCompound under an optionally byte array shape representing this block
+   */
   def nbt: Option[Array[Byte]]
 }
 
-abstract class BlockWrapper[A] extends Wrapper[A, Block] with Unwrapper[Block, A] {
+/**
+ * Wrapper for minecraft block to [[Block]] and [[Block]] to minecraft block.
 
-  def toCustom(block: A): Block = wrap(block)
-
-  def fromCustom(block: Block): A = unwrap(block)
-
-}
+ * @tparam A minecraft block type
+ * @version 0.0.1
+ * @author Warzou
+ */
+abstract class BlockWrapper[A] extends Wrapper[A, Block] with Unwrapper[Block, A]
